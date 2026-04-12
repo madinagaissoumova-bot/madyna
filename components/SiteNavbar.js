84d75ea1ddf25f9c1824d08e7677da34850e4203
gaissoumova madina <gaissoumova.madinaa@gmail.com>
2026-04-12 12:01:17 +0200

    Refine brand logo styling

--- components/SiteNavbar.js
+++ components/SiteNavbar.js
@@ -44,7 +44,9 @@ export default function SiteNavbar({
     <header className={`site-header${sticky ? " auth-header" : ""}`} id="top">
       <div className="container header-inner">
         <Link href="/" className="logo" aria-label="Mady Mode accueil" onClick={() => setMenuOpen(false)}>
-          <span className="logo-mark">M</span>
+          <span className="logo-mark">
+            <span className="logo-mark-inner">M</span>
+          </span>
           <span className="logo-text">
             <strong>Mady Mode</strong>
             <span>Mode modeste</span>
